53eddcaaf2196f5e7cc6158119565961b7c6b37e
Simon Clematide <simon.clematide@uzh.ch>
2025-02-04 22:40:59 +0100

    refactor setup.mk to improve clarity and organization; update variable comments and target definitions

--- setup.mk
+++ setup.mk
@@ -6,30 +6,31 @@ $(call log.debug, COOKBOOK BEGIN INCLUDE: cookbook/setup.mk)
 ###############################################################################
 
 
-
-
-# USERVARIABLE: BUILD_DIR
+# USER-VARIABLE: BUILD_DIR
 # The build directory where all local input and output files are stored
 # The content of BUILD_DIR can be removed anytime without issues regarding s3
 BUILD_DIR ?= build.d
 
-# PATTERN: %.d
+
+# PATTERN-RULE: %.d
 # Creates directory if it doesn't exist
 %.d:
 	mkdir -p $@
 
-# MULTITARGET: setup 
+
+# DOUBLE-COLON-TARGET: setup 
 #: Sets up the build directory and runs the active setup-<TARGET> targets
 setup:: | $(BUILD_DIR)
 
-# USERVARIABLE: OS
-# Detect the operating system
+
+# USER-VARIABLE: OS
+# Detect the operating system if not set from outside
 OS ?= $(shell uname -s)
   $(call log.debug, OS)
 
-#: VARIABLE: INSTALLER
-#: Defines the package manager for the operating system
 
+# VARIABLE: INSTALLER
+# Defines the package manager for the software installation on operating system level
 
 # If Linux, check the distribution
 ifeq ($(OS),Linux)
@@ -37,11 +38,13 @@ ifeq ($(OS),Linux)
     ifneq ($(DISTRO),)
         INSTALLER := apt
     endif
+
 # for MacOS we use brew
 else ifeq ($(OS),Darwin)
     INSTALLER := brew
 endif
-# if not set, let make complain about an undefined variable
+
+# if not set, let make complain about an undefined variable here
   $(call log.debug, INSTALLER)
 
 
@@ -51,5 +54,6 @@ update-pip-requirements-file:
 	pipenv requirements > requirements.txt
 
 PHONY_TARGETS += update-pip-requirements-file
+
 
 $(call log.debug, COOKBOOK END INCLUDE: cookbook/setup.mk)
