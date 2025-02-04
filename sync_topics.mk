d26e6454ee7cef2ad09225fc7161875b0587d0fb
Simon Clematide <simon.clematide@uzh.ch>
2025-02-04 10:35:06 +0100

    refactor sync_topics.mk to improve organization and clarity; update target names and streamline synchronization processes

--- sync_topics.mk
+++ sync_topics.mk
@@ -1,24 +1,20 @@
 $(call log.debug, COOKBOOK BEGIN INCLUDE: cookbook/sync_topics.mk)
 
+###############################################################################
+# SYNC TOPICS TARGETS
+# Targets for synchronizing processed topics data between S3 and local storage
+###############################################################################
 
 
+
+# The local per-newspaper synchronization file stamp for the output topics: What is on S3 has been synced?
 LOCAL_TOPICS_SYNC_STAMP_FILE := $(LOCAL_PATH_TOPICS).last_synced
   $(call log.debug, LOCAL_TOPICS_SYNC_STAMP_FILE)
 
-clean-sync-input:
-	rm -vf $(IN_LOCAL_PROCESSED_DATA_LINGPROC_LAST_SYNCED_FILE) || true
-	rm -rfv $(LOCAL_PATH_LINGPROC) || true
 
-PHONY_TARGETS += clean-sync-input
-
-
-#### SYNCING THE OUTPUT DATA FROM S3 TO LOCAL DIRECTORY
-sync-output:: sync-output-topics
-PHONY_TARGETS += sync-output-topics
-
-# the suffix of for the local stamp files (added to the input paths on s3)
-OUT_LOCAL_TOPICS_STAMP_SUFFIX ?= ''
-  $(call log.debug, OUT_LOCAL_TOPICS_STAMP_SUFFIX)
+# The suffix for the local stamp files (added to the local paths of s3 input paths)
+LOCAL_TOPICS_STAMP_SUFFIX ?= $(LOCAL_STAMP_SUFFIX)
+  $(call log.debug, LOCAL_TOPICS_STAMP_SUFFIX)
 
 # Rule to sync the output data from the S3 bucket to the local directory
 $(LOCAL_TOPICS_SYNC_STAMP_FILE):
@@ -26,42 +22,36 @@ $(LOCAL_TOPICS_SYNC_STAMP_FILE):
 	python lib/s3_to_local_stamps.py \
 	   $(S3_PATH_TOPICS) \
 	   --local-dir $(BUILD_DIR) \
-	   --stamp-extension $(OUT_LOCAL_TOPICS_STAMP_SUFFIX) \
+	   --stamp-extension $(LOCAL_TOPICS_STAMP_SUFFIX) \
 	   --logfile $@.log.gz && \
 	touch $@
 
-sync-output-topics: $(LOCAL_TOPICS_SYNC_STAMP_FILE)
 
-PHONY_TARGETS += sync-output-topics
+sync-topics: $(LOCAL_TOPICS_SYNC_STAMP_FILE)
+
+PHONY_TARGETS += sync-topics
+
+#### SYNCING THE OUTPUT DATA FROM S3 TO LOCAL DIRECTORY
+
+# MULTITARGET: sync-output
+# Synchronizes topics output data from/to S3
+sync-output:: sync-topics
+
+# MULTITARGET: sync-input
+sync-input:: sync-topics
 
 
 ### CLEANING THE SYNC OUTPUT
 
+# Target: clean-sync
+# Cleans the local synchronization files and directories
 clean-sync:: clean-sync-topics
-
-PHONY_TARGETS += clean-sync-topics
 
 clean-sync-topics:
 	rm -vf $(LOCAL_TOPICS_SYNC_STAMP_FILE) || true
+	rm -rfv $(LOCAL_PATH_TOPICS) || true
 
 PHONY_TARGETS += clean-sync-topics
-
-
-resync-output: clean-sync-output
-	$(MAKE) sync-output
-
-PHONY_TARGETS += resync-output
-
-
-resync-input: clean-sync-input
-	$(MAKE) sync-input
-
-PHONY_TARGETS += resync-input
-
-# Remove the local synchronization file stamp and redoes everything, ensuring a full sync with the remote server.
-resync: resync-input resync-output
-
-PHONY_TARGETS += resync
 
 
 
