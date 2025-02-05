d9022342a2e33821b35f85aba963a913092d71bb
Simon Clematide <simon.clematide@uzh.ch>
2025-02-05 23:58:00 +0100

    refactor processing_lingproc.mk to enhance documentation and organization; add user-variable descriptions for processing options

--- processing_lingproc.mk
+++ processing_lingproc.mk
@@ -1,105 +1,119 @@
 $(call log.debug, COOKBOOK BEGIN INCLUDE: cookbook/processing_lingproc.mk)
-
 ###############################################################################
 # LINGUISTIC PROCESSING TARGETS
 # Targets for processing newspaper content with linguistic analysis
 ###############################################################################
 
-# Configuration for S3 storage updates
-# Set to --validate for schema validation of output
+
+# USER-VARIABLE: LINGPROC_VALIDATE_OPTION
+# Option to enable schema validation of the output
+#
+# Set to no value or $(EMPTY) for preventing JSON schema validation
+# LINGPROC_VALIDATE_OPTION ?= $(EMPTY)
 LINGPROC_VALIDATE_OPTION ?= --validate
   $(call log.debug, LINGPROC_VALIDATE_OPTION)
 
-# Prevent any output to s3 even if s3-output-path is set
+# USER-VARIABLE: LINGPROC_S3_OUTPUT_DRY_RUN
+# Prevents output to S3 even if an S3 output path is set
+#
+# To enable dry-run mode use the following option:
 # LINGPROC_S3_OUTPUT_DRY_RUN?= --s3-output-dry-run
-# To disable the dry-run mode, comment the line above and uncomment the line below
 LINGPROC_S3_OUTPUT_DRY_RUN ?=
   $(call log.debug, LINGPROC_S3_OUTPUT_DRY_RUN)
 
-# Keep only the local timestam output files after uploading (only relevant when
-# uploading to s3)
+
+# USER-VARIABLE: LINGPROC_KEEP_TIMESTAMP_ONLY_OPTION
+# Retains only local timestamped output files after uploading
 #
+# To disable the keep-timestamp-only mode, use:
+# LINGPROC_KEEP_TIMESTAMP_ONLY_OPTION ?= 
 LINGPROC_KEEP_TIMESTAMP_ONLY_OPTION ?= --keep-timestamp-only
-# To disable the keep-timestamp-only mode, comment the line above and uncomment the line below
-#LINGPROC_KEEP_TIMESTAMP_ONLY_OPTION ?= 
+
   $(call log.debug, LINGPROC_KEEP_TIMESTAMP_ONLY_OPTION)
 
 
-# Quit the processing if the output file already exists in s3
-# double check if the output file exists in s3 and quit if it does
+# USER-VARIABLE: LINGPROC_QUIT_IF_S3_OUTPUT_EXISTS_OPTION
+# Stops processing if output file already exists on S3
+#
+# To disable, use:
+# LINGPROC_QUIT_IF_S3_OUTPUT_EXISTS_OPTION ?= 
 LINGPROC_QUIT_IF_S3_OUTPUT_EXISTS_OPTION ?= --quit-if-s3-output-exists
-# To disable the quit-if-s3-output-exists mode, comment the line above and uncomment the line below
-#LINGPROC_QUIT_IF_S3_OUTPUT_EXISTS_OPTION ?=
   $(call log.debug, LINGPROC_QUIT_IF_S3_OUTPUT_EXISTS_OPTION)
 
-# @TODO: Add a quiet option to the processing script
+
+# USER-VARIABLE: LINGPROC_QUIET_OPTION
+# Reserved for quiet processing mode (@TODO: Implement in script)
 LINGPROC_QUIET_OPTION ?= 
   $(call log.debug, LINGPROC_QUIET_OPTION)
 
-# variable for all locally available rebuilt stamp files. Needed for dependency tracking
-# of the build process. We discard errors as the path or file might not exist yet.
+
+# VARIABLE: LOCAL_REBUILT_STAMP_FILES
+# Stores all locally available rebuilt stamp files for dependency tracking
 LOCAL_REBUILT_STAMP_FILES := \
     $(shell ls -r $(LOCAL_PATH_REBUILT)/*.jsonl.bz2$(LOCAL_REBUILT_STAMP_SUFFIX) 2> /dev/null \
     | $(if $(NEWSPAPER_YEAR_SORTING),$(NEWSPAPER_YEAR_SORTING),cat))
   $(call log.debug, LOCAL_REBUILT_STAMP_FILES)
 
 
-
-# make function to convert a local rebuilt file name to a local lingproc file name 
+# FUNCTION: LocalRebuiltToLingprocFile
+# Converts a local rebuilt file name to a local linguistic processing file name
 define LocalRebuiltToLingprocFile
 $(1:$(LOCAL_PATH_REBUILT)/%.jsonl.bz2$(LOCAL_REBUILT_STAMP_SUFFIX)=$(LOCAL_PATH_LINGPROC)/%.jsonl.bz2)
 endef
 
 
+# VARIABLE: LOCAL_LINGPROC_FILES
+# Stores the list of linguistic processing files based on rebuilt stamp files
 LOCAL_LINGPROC_FILES := \
     $(call LocalRebuiltToLingprocFile,$(LOCAL_REBUILT_STAMP_FILES))
 
   $(call log.debug, LOCAL_LINGPROC_FILES)
 
-# Note: make sync is needed in a separate process to prepare the data for the build! This target just takes whatever the
-# current situation regarding the data is and processes it. It does not sync the data from s3 to the local directory.
+# TARGET: lingproc-target
+#: Processes newspaper content with linguistic analysis
+#
+# Just uses the local data that is there, does not enforce synchronization
 lingproc-target: $(LOCAL_LINGPROC_FILES)
 
-.PHONY: lingproc-target
+PHONY_TARGETS += lingproc-target
 
-
-# Rule to process a single newspaper
-# Note: we need to unset the errexit SHELL flag to be able to communicate the exit code of the processing script
-$(LOCAL_PATH_LINGPROC)/%.jsonl.bz2: $(LOCAL_PATH_REBUILT)/%.jsonl.bz2.stamp $(LOCAL_PATH_LANGIDENT)/%.jsonl.bz2
+# FILE-RULE: $(LOCAL_PATH_LINGPROC)/%.jsonl.bz2
+#: Rule to process a single newspaper
+#
+# Note: Unsets errexit flag to communicate exit codes
+$(LOCAL_PATH_LINGPROC)/%.jsonl.bz2: $(LOCAL_PATH_REBUILT)/%.jsonl.bz2$(LOCAL_REBUILT_STAMP_SUFFIX) $(LOCAL_PATH_LANGIDENT)/%.jsonl.bz2
 	$(MAKE_SILENCE_RECIPE) \
 	mkdir -p $(@D) && \
 	{  set +e ; \
-	  python3 lib/spacy_linguistic_processing.py \
-          $(call local_to_s3,$<,.stamp) \
-          --lid $(call local_to_s3,$(word 2,$^),.stamp) \
+     python3 lib/spacy_linguistic_processing.py \
+          $(call local_to_s3,$<,$(LOCAL_REBUILT_STAMP_SUFFIX)) \
+          --lid $(call local_to_s3,$(word 2,$^),'') \
           $(LINGPROC_VALIDATE_OPTION) \
-          --s3-output-path $(call local_to_s3,$@,.stamp) \
+          --s3-output-path $(call local_to_s3,$@,.'') \
           $(LINGPROC_KEEP_TIMESTAMP_ONLY_OPTION) \
           $(LINGPROC_QUIT_IF_S3_OUTPUT_EXISTS_OPTION) \
           $(LINGPROC_S3_OUTPUT_DRY_RUN) \
           $(LINGPROC_QUIET_OPTION) \
-		  --git-version $(git_version) \
+          --git-version $(git_version) \
           -o $@ \
           --log-file $@.log.gz ; \
     EXIT_CODE=$$? ; \
-	echo "Processing exit code: $$EXIT_CODE" ; \
-    if [ $$EXIT_CODE -eq 0 ] ; then \
-        echo "Processing completed successfully. Uploading logfile..." ; \
-        python3 lib/s3_to_local_stamps.py \
-            $(call local_to_s3,$@,.stamp).log.gz \
-            --upload-file $@.log.gz \
-			--force-overwrite ; \
-    elif [ $$EXIT_CODE -eq 3 ] ; then \
-        echo "Processing skipped (output exists on S3). Not uploading logfile." ; \
-        rm -f $@ ; \
-        exit 0 ; \
-    else \
-        echo "An error occurred during processing. Exit code: $$EXIT_CODE" ; \
-        rm -f $@ ; \
-        exit $$EXIT_CODE ; \
-    fi ; }
+    echo "Processing exit code: $$EXIT_CODE" ; \
+      if [ $$EXIT_CODE -eq 0 ] ; then \
+          echo "Processing completed successfully. Uploading logfile..." ; \
+          python3 lib/s3_to_local_stamps.py \
+              $(call local_to_s3,$@,.stamp).log.gz \
+              --upload-file $@.log.gz \
+        --force-overwrite ; \
+      elif [ $$EXIT_CODE -eq 3 ] ; then \
+          echo "Processing skipped (output exists on S3). Not uploading logfile." ; \
+          rm -f $@ ; \
+          exit 0 ; \
+      else \
+          echo "An error occurred during processing. Exit code: $$EXIT_CODE" ; \
+          rm -f $@ ; \
+          exit $$EXIT_CODE ; \
+      fi ; }
 
-
-	  
 
 $(call log.debug, COOKBOOK END INCLUDE: cookbook/processing_lingproc.mk)
