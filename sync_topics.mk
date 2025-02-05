8b5fc12df58b1d8b61d0f002c33d6f2b059dcf86
Simon Clematide <simon.clematide@uzh.ch>
2025-02-05 22:44:13 +0100

    refactor sync_topics.mk to improve target definitions and organization; add double colon specifications for sync-input and sync-output targets

--- sync_topics.mk
+++ sync_topics.mk
@@ -1,58 +1,56 @@
 $(call log.debug, COOKBOOK BEGIN INCLUDE: cookbook/sync_topics.mk)
-
 ###############################################################################
 # SYNC TOPICS TARGETS
 # Targets for synchronizing processed topics data between S3 and local storage
 ###############################################################################
 
 
+# DOUBLE-COLON-TARGET: sync-output
+# Synchronizes topic processing output data from/to S3
+sync-output :: sync-topics
 
-# The local per-newspaper synchronization file stamp for the output topics: What is on S3 has been synced?
+
+# DOUBLE-COLON-TARGET: sync-input
+# Synchronizes linguistic processing input data from S3
+sync-input :: sync-topics
+
+
+# VARIABLE: LOCAL_TOPICS_SYNC_STAMP_FILE
+# Local stamp file to track synchronization status
 LOCAL_TOPICS_SYNC_STAMP_FILE := $(LOCAL_PATH_TOPICS).last_synced
   $(call log.debug, LOCAL_TOPICS_SYNC_STAMP_FILE)
 
 
-# The suffix for the local stamp files (added to the local paths of s3 input paths)
-LOCAL_TOPICS_STAMP_SUFFIX ?= $(LOCAL_STAMP_SUFFIX)
+# TARGET: sync-topics
+#: Synchronizes topics processing data
+sync-topics : $(LOCAL_TOPICS_SYNC_STAMP_FILE)
+
+PHONY_TARGETS += sync-topics
+
+
+# USER-VARIABLE: LOCAL_TOPICS_STAMP_SUFFIX
+# The suffix for local stamp files (added to the input paths on S3)
+LOCAL_TOPICS_STAMP_SUFFIX ?= ''
   $(call log.debug, LOCAL_TOPICS_STAMP_SUFFIX)
 
-# Rule to sync the output data from the S3 bucket to the local directory
+
+# FILE-RULE: LOCAL_TOPICS_SYNC_STAMP_FILE
+#: Rule to sync the output data from the S3 bucket to the local directory
 $(LOCAL_TOPICS_SYNC_STAMP_FILE):
 	mkdir -p $(@D) && \
 	python lib/s3_to_local_stamps.py \
 	   $(S3_PATH_TOPICS) \
 	   --local-dir $(BUILD_DIR) \
 	   --stamp-extension $(LOCAL_TOPICS_STAMP_SUFFIX) \
-	   --logfile $@.log.gz && \
+	   2> >(tee $@.log >&2) && \
 	touch $@
 
 
-sync-topics: $(LOCAL_TOPICS_SYNC_STAMP_FILE)
-
-PHONY_TARGETS += sync-topics
-
-#### SYNCING THE OUTPUT DATA FROM S3 TO LOCAL DIRECTORY
-
-# MULTITARGET: sync-output
-# Synchronizes topics output data from/to S3
-sync-output:: sync-topics
-
-# MULTITARGET: sync-input
-sync-input:: sync-topics
-
-
-### CLEANING THE SYNC OUTPUT
-
-# Target: clean-sync
-# Cleans the local synchronization files and directories
-clean-sync:: clean-sync-topics
-
+# TARGET: clean-sync-topics
+#: Removes synchronized topic data from local storage
 clean-sync-topics:
-	rm -vf $(LOCAL_TOPICS_SYNC_STAMP_FILE) || true
-	rm -rfv $(LOCAL_PATH_TOPICS) || true
+	rm -rfv $(LOCAL_PATH_TOPICS) $(LOCAL_TOPICS_SYNC_STAMP_FILE) || true
 
 PHONY_TARGETS += clean-sync-topics
-
-
 
 $(call log.debug, COOKBOOK END INCLUDE: cookbook/sync_topics.mk)
