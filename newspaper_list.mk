8f3f49e9da671456687b06720fa7b7dcdf42c795
Simon Clematide <simon.clematide@uzh.ch>
2025-02-05 22:50:45 +0100

    refactor newspaper_list.mk for improved documentation and organization; add user-variable descriptions and enhance processing order configuration

--- newspaper_list.mk
+++ newspaper_list.mk
@@ -3,39 +3,55 @@ $(call log.debug, COOKBOOK BEGIN INCLUDE: cookbook/newspaper_list.mk)
 ###############################################################################
 # NEWSPAPER LIST MANAGEMENT
 # Configuration and generation of newspaper processing lists
+#
+# This Makefile sets up the configuration and processing order of newspaper
+# lists retrieved from an S3 bucket. The list is either shuffled or kept in
+# chronological order, based on user settings.
 ###############################################################################
 
-# Default to some newspaper if none is specified
+# USER-VARIABLE: NEWSPAPER
+# Default newspaper selection if none is specified
 NEWSPAPER ?= actionfem
   $(call log.debug, NEWSPAPER)
 
+
+# USER-VARIABLE: NEWSPAPERS_TO_PROCESS_FILE
 # Configuration file containing space-separated newspapers to process
 NEWSPAPERS_TO_PROCESS_FILE ?= $(BUILD_DIR)/newspapers.txt
   $(call log.debug, NEWSPAPERS_TO_PROCESS_FILE)
 
-# Processing order configuration
-# Set to 'shuf' for random order or 'cat' for chronological
+
+# USER-VARIABLE: NEWSPAPER_YEAR_SORTING
+# Determines the order of newspaper processing
+# - 'shuf' for random order
+# - 'cat' for chronological order
 NEWSPAPER_YEAR_SORTING ?= shuf
   $(call log.debug, NEWSPAPER_YEAR_SORTING)
 
-# TARGET: newspaper-list-target
-# Generates list of newspapers to process from S3 bucket
-newspaper-list-target: $(NEWSPAPERS_TO_PROCESS_FILE)
-PHONY_TARGETS += newspaper-list-target
 
-
+# USER-VARIABLE: S3_PREFIX_NEWSPAPERS_TO_PROCESS_BUCKET
+# S3 bucket prefix containing newspapers for processing
 S3_PREFIX_NEWSPAPERS_TO_PROCESS_BUCKET ?= $(S3_BUCKET_REBUILT)
   $(call log.debug, S3_PREFIX_NEWSPAPERS_TO_PROCESS_BUCKET)
 
 
-# Rule to generate the file containing the newspapers to process
-# we shuffle the newspapers to avoid recomputations by different machines working on the dataset
+# TARGET: newspaper-list-target
+#: Generates a list of newspapers to process from the S3 bucket
+newspaper-list-target: $(NEWSPAPERS_TO_PROCESS_FILE)
+PHONY_TARGETS += newspaper-list-target
+
+
+# FILE-RULE: $(NEWSPAPERS_TO_PROCESS_FILE)
+#: Generates the file containing the newspapers to process
+#
+# This rule retrieves the list of available newspapers from an S3 bucket,
+# shuffles them to distribute processing evenly, and writes them to a file.
 $(NEWSPAPERS_TO_PROCESS_FILE): | $(BUILD_DIR)
 	python -c \
 	"import boto3, os, random; \
-	s3 =boto3.resource( \
+	s3 = boto3.resource( \
         's3',\
-        aws_secret_access_key= os.getenv('SE_SECRET_KEY'),\
+        aws_secret_access_key=os.getenv('SE_SECRET_KEY'),\
         aws_access_key_id=os.getenv('SE_ACCESS_KEY'),\
         endpoint_url=os.getenv('SE_HOST_URL'))\
 	bucket = s3.Bucket('$(S3_PREFIX_NEWSPAPERS_TO_PROCESS_BUCKET)'); \
@@ -44,6 +60,5 @@ $(NEWSPAPERS_TO_PROCESS_FILE): | $(BUILD_DIR)
 	random.shuffle(l); \
     print(*l)" \
 	> $@
-
 
 $(call log.debug, COOKBOOK END INCLUDE: cookbook/newspaper_list.mk)
