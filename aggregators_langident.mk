4c694c084877264e7ccf7ebb374d80669031800b
Simon Clematide <simon.clematide@uzh.ch>
2025-06-30 02:32:48 +0200

    fix: update S3 prefix formatting in aggregate target for proper file naming

--- aggregators_langident.mk
+++ aggregators_langident.mk
@@ -1,5 +1,5 @@
 
 aggregate:
 	python cookbook/lib/s3_aggregator.py --jq-filter cookbook/lib/langident_stats.jq \
-	--s3-prefix $(S3_PATH_LANGIDENT) \
-	-o $(S3_PATH_LANGIDENT)__AGGREGATED.jsonl.gz 
+	--s3-prefix $(S3_PATH_LANGIDENT:/$(NEWSPAPER)=) \
+	-o $(S3_PATH_LANGIDENT:/$(NEWSPAPER)=)__AGGREGATED.jsonl.gz 
