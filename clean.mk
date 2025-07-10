7b63e52faa8ce4fa136dc28a77c660eb784f6a31
Simon Clematide <simon.clematide@uzh.ch>
2025-07-10 08:47:08 +0200

    feat: enhance clean targets with detailed echo statements for better clarity

--- clean.mk
+++ clean.mk
@@ -5,25 +5,44 @@ $(call log.debug, COOKBOOK BEGIN INCLUDE: cookbook/clean.mk)
 # Targets for cleaning build artifacts and sync status
 ###############################################################################
 
+clean :: clean-build
+
+.PHONY: clean
+
+help::
+	@echo "  clean            # Cleans the build directory and all processed data"
+
 
 # TARGET: clean-build
 #: Removes entire build directory and all processed data
 clean-build:
+	# Removing build directory and all processed data in $(BUILD_DIR)/...
 	rm -rvf $(BUILD_DIR) || true
+	# Finished cleaning build directory
 
 .PHONY: clean-build
+
+# DOUBLE-COLON-TARGET: clean-sync
+#: Removes locally synced materials from S3
+clean-sync :: clean-sync-input clean-sync-output
+
+.PHONY: clean-sync
+
+help::
+	@echo "  clean-sync         # Removes locally synced materials from S3"
+
 
 # TARGET: clean-sync-input
 #: Removes locally synced materials from S3
 clean-sync-input ::
-	# Removing synchronized input data...
+	# Removing synchronized input data in $(BUILD_DIR)/...
 
 .PHONY: clean-sync-input
 
 # TARGET: clean-sync-output
 #: Removes locally synced materials from S3
 clean-sync-output ::
-	# Removing synchronized output data...
+	# Removing synchronized output data in in $(BUILD_DIR)/...
 
 .PHONY: clean-sync-output
 
