154ce1c6c0d6bb95913d88c356cb41f1781e7968
Simon Clematide <simon.clematide@uzh.ch>
2025-10-16 14:14:55 +0200

    Adds aggregation for image dimensions in bboxqa cookbook

--- aggregators_bboxqa.mk
+++ aggregators_bboxqa.mk
@@ -9,6 +9,10 @@ aggregate-iiif-errors:
 	--s3-prefix s3://$(S3_BUCKET_BBOXQA)/$(PROCESS_LABEL_BBOXQA)/$(RUN_ID_BBOXQA) \
 	-o s3://$(S3_BUCKET_BBOXQA)/$(PROCESS_LABEL_BBOXQA)/$(RUN_ID_BBOXQA)__AGGREGATED_iiif-errors.jsonl.gz 
 
+aggregate-page-dimensions:
+	python cookbook/lib/s3_aggregator.py --jq-filter lib/image-dimensions.jq \
+	--s3-prefix s3://$(S3_BUCKET_BBOXQA)/$(PROCESS_LABEL_BBOXQA)/$(RUN_ID_BBOXQA) \
+	-o s3://$(S3_BUCKET_BBOXQA)/$(PROCESS_LABEL_BBOXQA)/$(RUN_ID_BBOXQA)__AGGREGATED_image-dimensions.jsonl.gz
 
 aggregate: aggregate-pagestats aggregate-iiif-errors
  #python s3_aggregator.py -k ci_id ocrqa --s3-prefix s3://42-processed-data-final/ocrqa/ocrqa-ocrqa-wp_v1.0.6_v1-0-0/ -o 3://42-processed-data-final/ocrqa/ocrqa-ocrqa-wp_v1.0.6_v1-0-0__AGGREGATED.jsonl.gz 
