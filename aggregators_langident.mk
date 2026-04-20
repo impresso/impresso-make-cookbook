6e5029f227b48f3f796086ea56d3fc2740031d5f
Simon Clematide <simon.clematide@uzh.ch>
2026-04-20 14:02:50 +0200

    feat: include source metadata in aggregation process for langident

--- aggregators_langident.mk
+++ aggregators_langident.mk
@@ -1,6 +1,7 @@
 
 aggregate:
 	python cookbook/lib/s3_aggregator.py --jq-filter cookbook/lib/langident_stats.jq \
+	--include-source-meta \
 	--s3-prefix $(S3_PATH_LANGIDENT:/$(NEWSPAPER)=) \
 	-o $(S3_PATH_LANGIDENT:/$(NEWSPAPER)=)__AGGREGATED.jsonl.gz 
 
