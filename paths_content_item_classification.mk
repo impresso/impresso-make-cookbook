fdf610f1f2a3ba69d2604f012752f2778292963b
Simon Clematide <simon.clematide@uzh.ch>
2026-06-08 15:28:53 +0200

    fix: correct HF model format and remove unsupported :v2.0 syntax
    - Remove :v2.0 suffix as HuggingFace doesn't support version in model name
    - AdClassifierPipeline doesn't expose revision parameter
    - Default to base model: impresso-project/impresso-multilingual-ad-classification
    - Document that specific versions require either separate model repos or
      enhancement to AdClassifierPipeline to support revision parameter

--- paths_content_item_classification.mk
+++ paths_content_item_classification.mk
@@ -48,9 +48,11 @@ MODEL_ID_content_item_classification ?= multilingual
 # USER-VARIABLE: HF_MODEL_CONTENT_ITEM_CLASSIFICATION
 # HuggingFace model identifier for ad classification
 #
-# Specifies the full HuggingFace model path/tag including version.
-# Default uses v2.0 of the impresso ad classifier.
-HF_MODEL_CONTENT_ITEM_CLASSIFICATION ?= impresso-project/impresso-multilingual-ad-classification:v2.0
+# Specifies the full HuggingFace model repository path.
+# To use a specific version/revision, you may need to update the impresso-pipelines package
+# to support the 'revision' parameter, or use a model name that includes the version.
+# Default: impresso-project/impresso-multilingual-ad-classification
+HF_MODEL_CONTENT_ITEM_CLASSIFICATION ?= impresso-project/impresso-multilingual-ad-classification
   $(call log.debug, HF_MODEL_CONTENT_ITEM_CLASSIFICATION)
 
 
